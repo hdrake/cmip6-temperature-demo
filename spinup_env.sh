85d4e7b5676de7196b2d0fea3ef6a937fcd74972
Henri Drake <henrifdrake@gmail.com>
2020-01-05 21:02:49 +0000

    Updated notbeooks and python environment

---
+++ spinup_env.sh
@@ -0,0 +1,4 @@
+conda env update --file environment.yml < y.txt
+conda activate cmip6-temperature-demo
+python -m ipykernel install --user --name cmip6-temperature-demo --display-name cmip6-temperature-demo
+
